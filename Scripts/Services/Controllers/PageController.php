19831f7a29baa2ad651151ec820c691db6c22221
ant1quariat <zagorodneff.kirill@gmail.com>
2022-08-23 14:59:42 +0400

    Ver 2.1

---
+++ Scripts/Services/Controllers/PageController.php
@@ -0,0 +1,17 @@
+<?php
+    
+function Home() {
+    require 'Public/Pages/Home.php';
+}
+function News() {
+    require 'Public/Pages/Home.php';
+}
+function Profile() {
+    require 'Public/Pages/Home.php';
+}
+function Group() {
+    require'Public/Pages/Home.php';
+}
+function Discussion() {
+    require 'Public/Pages/Home.php';
+}
--- index.php
+++ index.php
@@ -1,5 +1,69 @@
-<?php
+<!DOCTYPE html>
+<!-- В зависимости от data-theme меняется цветовая палитра.  -->
+<html lang="ru" data-theme="light"> 
 
-require_once 'Scripts/App.php';
+<!-- Подключение head -->
+<?php include_once 'Assets/Components/Headers/hd-head.php'; ?>
 
-url_route();
+<body>
+    <!-- Подключение хедера  -->
+    <?php include_once 'Assets/Components/Headers/hd-header.php'; ?>
+
+    <section class="layout">
+
+        <!-- Подключение меню -->
+        <?php require 'Assets/Components/Menu/nv-menu.php'; ?>
+
+        <main class="content">
+            <?php
+                require_once 'Scripts/Services/Controllers/PageController.php';
+                require_once 'Scripts/Services/Router/Route.php';
+                
+                use Scripts\Services\Router\Route;
+                
+                $router = new Route();
+
+                $router->map('GET','/', function(){
+                    include 'Public/Pages/Home.php';
+                });
+                $router->map('GET','/news', function(){
+                    include 'Public/Pages/News.php';
+                });
+                $router->map('GET','/profile', function(){
+                    include 'Public/Pages/Profile.php';
+                });
+                $router->map('GET','/group', function(){
+                    include 'Public/Pages/Group.php';
+                });
+                $router->map('GET','/discussion', function(){
+                    include 'Public/Pages/Discussion.php';
+                });
+                $router->map('GET','/API/Forms', function(){
+                    echo 'Invalid method';
+                });
+                $router->map('POST','/API/Forms', function(){
+                    include_once 'Public/Errors/404.php';
+                });
+                
+
+                $match = $router->match();
+                if (is_array($match)) {
+                    if (is_callable($match['target']))
+                        call_user_func_array($match['target'], $match['params']);
+                    else if (is_array($match['target']) && file_exists($adminDir."/Controllers/".$match['target'][0].'.php')) {
+                        require_once $adminDir . "/Controllers/" . $match['target'][0] . '.php';
+                        $nameCl = $match['target'][0];
+                        $nameFunc = $match['target'][1];
+                        $controller = new $nameCl();
+                        $controller->$nameFunc($match['params']);
+                    }
+                } else {
+                    include_once 'Public/Errors/404.php';
+                }
+
+            ?>
+        </main>
+    </section>
+
+</body>
+</html>
