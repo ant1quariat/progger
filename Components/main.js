3c626946c1c16561187bc48f6d730ebe7c03bbcc
ant1quariat <zagorodneff.kirill@gmail.com>
2022-08-28 23:14:10 +0400

    Version 2.3

--- Components/main.js
+++ Components/main.js
@@ -76,3 +76,119 @@ selectItems.forEach(function(items) {
 
     })
 });
+
+const themeSwitch = document.getElementById('theme-switch');
+const themeAttr = document.querySelector('body');
+
+themeSwitch.addEventListener('click', function() {
+    if (themeAttr.getAttribute('theme') === "dark"){
+        themeAttr.setAttribute('theme', "light")
+    } else {
+        themeAttr.setAttribute('theme', "dark")
+    }
+});
+
+
+const cb = document.getElementById('check-1');
+
+cb.addEventListener('change', function() {
+
+    // if (localStorage.getItem('theme') === "dark"){
+    //     themeAttr.setAttribute('theme', "dark")
+    // } else {
+    //     themeAttr.setAttribute('theme', "light")
+    // }
+
+    if (localStorage.getItem('theme') === "dark"){
+
+        themeAttr.setAttribute('theme', "light");
+        
+        localStorage.setItem('theme', "light");
+
+    } else {
+
+        themeAttr.setAttribute('theme', "dark");
+
+        localStorage.setItem('theme', "light");
+    }
+})
+
+
+
+class Stogage {
+    constructor(name) {
+
+      this.name = name;
+
+      this.hash = {};
+
+      let text = localStorage.getItem(this.name);
+      if (text)
+        this.hash = JSON.parse(text);
+
+      this.save();
+
+    }
+
+    get(id) {
+
+      return this.item.find(item => item.id === id)
+
+    }
+
+    add(id, data) {
+
+      this.hash[id] = data;
+      this.save();
+
+    }
+
+    del(id) {
+
+      delete this.hash[id];
+      this.save();
+
+    }
+
+    save() {
+
+      this.list = Object.values(this.hash);
+
+      const text = JSON.stringify(this.hash);
+
+      localStorage.setItem(this.name, text);
+
+    }
+
+  }
+
+  const checkbox_store = new Stogage('checkbox_store');
+
+  checkbox_store.list.forEach(item => {
+
+    if (item.state === "on"){
+      return document.querySelector('#' + item.id).checked = item.state;
+
+    }
+
+    checkbox_store.del(item.id);
+
+  });
+
+  function changeHandler(event) {
+
+    let id = event.currentTarget.id;
+    let state = event.currentTarget.checked ? "on" : undefined;
+
+    checkbox_store.add(id, {
+        id: id,
+        state: state
+    });
+
+  };
+
+  document.querySelectorAll('.check').forEach(function (item) {
+    
+    item.addEventListener("change", changeHandler);
+
+  });
